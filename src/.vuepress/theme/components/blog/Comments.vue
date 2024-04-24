f0d0d2dc0ec6453750e9b94e2ac85d7d53cbbcf1
Dave Huseby <dwh@linuxprogrammer.org>
2024-04-23 20:29:44 -0600

    fix ssr of embed js
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- src/.vuepress/theme/components/blog/Comments.vue
+++ src/.vuepress/theme/components/blog/Comments.vue
@@ -5,15 +5,31 @@
   </div>
 </template>
 
-<script type="text/javascript">
-DiscourseEmbed = {
-  discourseUrl: 'https://discuss.libp2p.io/',
-  discourseEmbedUrl: 'EMBED_URL',
-};
-
-(function() {
-  var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
-  d.src = DiscourseEmbed.discourseUrl + 'javascript/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
-})();
+<script>
+const safePermalink = (permalink, date) => {
+  let domain = 'https://blog.libp2p.io/'
+  const url = new URL(domain)
+  url.pathname = permalink
+  return url.toString()
+}
+export default {
+  name: 'Comments',
+  components: {},
+  computed: {
+    embedSrc() {
+      return `https://discuss.libp2p.io/embed/comments?embed_url=${safePermalink(this.$frontmatter.permalink, this.$frontmatter.date)}`
+    },
+  },
+  mounted() {
+    window.DiscourseEmbed = {
+      discourseUrl: 'https://discuss.libp2p.io/',
+      discourseEmbedUrl: safePermalink(this.$frontmatter.permalink, this.$frontmatter.date),
+    }
+    const d = document.createElement('script')
+    d.type = 'text/javascript'
+    d.async = true
+    d.src = window.DiscourseEmbed.discourseUrl + 'javascripts/embed.js'
+    document.getElementsByTagName('body')[0].appendChild(d)
+  },
+}
 </script>
